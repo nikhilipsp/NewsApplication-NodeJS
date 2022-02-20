d0922a6c1e47f166fc52d90ff9f1eae209583a2a
Nikhil kumar <nikhil@dilligentech.com>
2022-02-20 15:41:29 +0530

    first commit

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ require('dotenv').config()
 var app = require('./app');
 const config = require('config');
 const { service: { port, name } } = config;
-// const logger = require('@dilligentech/logger');
-// const utils = require('@dilligentech/utils');
 
 app.listen(port, async () => {
 	try {
 		var start_message = "**** ApplicationServer " + name.toUpperCase() + " started running on port " + port + "****"
 		console.log(start_message)
-		//logger.info(start_message)
 	} catch (error) {
 		console.log("ApplicationServer Error ", error);
-	//	logger.error("ApplicationServer Error ", error)
 		process.exit(-1);
 	}
 });
